88a323dedf55c4a4357a3864217c2a274ab4aa19
Mohammad-thanzil <thanzilthanzil.501@gmail.com>
2023-11-06 18:15:59 +0530

    .changes

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 const billAmount =  document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#check-button");
-const message = document.querySelector("#error-message");
+const message = document.querySelector("#error-messege");
 const noOfNotes = document.querySelectorAll(".no-of-notes");
 
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
-checkButton.addEventListener("click", function validateBillAndCashAmount(){
-    hideMessage();
-    if (billAmount.value > 0){
-        if(cashGiven.value >= billAmount.value){
-            const amountToBeReturned = cashGiven.value - billAmount.value;
-            calculateChange(amountToBeReturned);
-        }else{
-            showMessage("Paise De Bhadwe Warva Aa Kam pe Laga Isko");
-        }
-    }else{
-       showMessage("Invalid Bill Amount");
 
+checkButton.addEventListener("click", function validateBillAndCashAmount() {
+    hideMessage();
+    const billAmountValue = parseFloat(billAmount.value);
+    const cashGivenValue = parseFloat(cashGiven.value);
+
+    if (billAmountValue <= 0 || isNaN(billAmountValue)) {
+        showMessage("Invalid Bill Amount");
+    } else if (cashGivenValue <= 0 || isNaN(cashGivenValue)) {
+        showMessage("Invalid Cash Given");
+    } else if (cashGivenValue < billAmountValue) {
+        showMessage("Give the balance amount BSDK.");
+    } else {
+        const amountToBeReturned = cashGivenValue - billAmountValue;
+        calculateChange(amountToBeReturned);
     }
 });
+
 
 function calculateChange(amountToBeReturned){
     // go over all the available
@@ -28,7 +32,11 @@ function calculateChange(amountToBeReturned){
         const numberOfNotes = Math.trunc(
             amountToBeReturned / availableNotes[i]
         );
-        amountToBeReturned %= availableNotes[i];
+        // amount left after calculating the number of notes needed
+        amountToBeReturned = amountToBeReturned % availableNotes[i];
+    
+    
+        //Updating the no of notes in the lable for the current amount   
         noOfNotes[i].innerText = numberOfNotes;
     }
 }
@@ -41,3 +49,6 @@ function showMessage(msg) {
     message.style.display = "block";
     message.innerText = msg;
 }
+
+
+
